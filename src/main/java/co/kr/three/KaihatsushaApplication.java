194c944c6fa2ced98acc98dc19ddf483c45076a0
son <wnstnals2007@naver.com>
2026-05-04 11:43:06 +0900

    spring project start

---
+++ src/main/java/co/kr/three/KaihatsushaApplication.java
@@ -0,0 +1,13 @@
+package co.kr.three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaihatsushaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaihatsushaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/kr/three/KaihatsushaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaihatsushaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
